87a345a103fb5d147b5dbe55dfa808256900625e
haiyen09clc <haiyen09clc@gmail.com>
2019-04-28 20:56:36 +0700

    Add a new file plugin.php

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: IZW Auto Complete Checkout
+Plugin URI: https://izweb.biz
+Description: Populates full name field in woocommerce checkout with personnel database in TablePress
+Version: 1.0.0
+Author: Sy Le
+Author URI:
+Author email: levansy@gmail.com
+Text Domain: greentech
+Domain Path: /i18n
+License: GPL 2
+*/
+/**
+ * IZW Auto Complete Class
+ */
+if (!class_exists('IZW_Auto_Complete')) {
+    class IZW_Auto_Complete
+    {
+        function __construct()
+        {
+
+        }
+    }
+    new IZW_Auto_Complete();
+}
