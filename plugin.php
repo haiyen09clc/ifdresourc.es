88fd884f3f0c8c8ad1d8a39dde176ca66889ab75
haiyen09clc <haiyen09clc@gmail.com>
2019-04-29 10:59:28 +0700

    Issue #5: Order restrictions per user per product in a specific time range

--- plugin.php
+++ plugin.php
@@ -41,5 +41,222 @@ if (!class_exists('IZW_Auto_Complete')) {
             add_filter( 'woocommerce_loop_add_to_cart_link', array( $this,'change_text_add_to_cart_button'), 10, 2 );
         }
     }
+    function check_order_limit($posted, $errors_obj){
+        $errors = array();
+        //get order items
+        if ( ! WC()->cart->is_empty() ) {
+            $product_ids = array();
+            foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+                $product = wc_get_product($cart_item['product_id']);
+                //variation_id -> red clothes, product_id -> clothes
+                if ($product->get_meta( '_product_rules' ) == 'yes') {
+                    if (isset($cart_item['variation_id']) && $product->get_meta('_variations') == 'no') {
+                        $product_ids[] = $cart_item['variation_id'];
+                        $check_id = $cart_item['variation_id'];
+                    } else {
+                        $product_ids[] = $cart_item['product_id'];
+                        $check_id = $cart_item['product_id'];
+                    }
+
+                    //$purchase_data[$cart_item['product_id']]['qty'] = $cart_item['quantity'];
+                    $purchase_data[$check_id]['allow'] = $product->get_meta('_product_quantity');
+                    $purchase_data[$check_id]['month'] = $product->get_meta('_month');
+                    $purchase_data[$check_id]['variations'] = $product->get_meta('_variations');
+                    $purchase_data[$check_id]['qty'] = $cart_item['quantity'];
+                    $purchase_data[$check_id]['remain'] = (int)$purchase_data[$check_id]['allow'] - $purchase_data[$check_id]['qty'];
+                    // echo $purchase_data[$check_id]['remain'];
+                    if ($purchase_data[$check_id]['remain']<0){
+                        $errors_obj->add( 'validation', sprintf( __( 'Pemmission denied. Per our rules you can buy %s %s per %s month(s)!', 'woocommerce' ), '<strong>' .$purchase_data[$check_id]['allow'] . '</strong>','<strong>' . $product->get_name(). '</strong>','<strong>' .$purchase_data[$check_id]['month']. '</strong>' ) );
+                    }
+
+                }
+            }
+        }
+
+
+        //get orders of that $name
+        global $wpdb;
+        $name = $posted['billing_first_name'];
+        $orders = $wpdb->get_col('SELECT post_id FROM '.$wpdb->postmeta.' WHERE meta_key="_billing_first_name" AND meta_value="'.$name.'" ORDER BY meta_id DESC');
+
+        //loop to get all products from orders
+        if (!empty($orders))
+            foreach ($orders as $order_id) {
+                $order = wc_get_order($order_id);
+
+                //get products of order
+                foreach ( $order->get_items() as $item ) {
+                    if ( ! $item->is_type( 'line_item' ) ) {
+                        continue;
+                    }
+
+                    $product            = $item->get_product();
+                    $product_id         = $product->get_type() == 'variation' ?$product->get_parent_id():$product->get_id();
+                    $variation_id       = $item->get_variation_id();
+                    if (in_array($product_id, $product_ids)) {
+                        $check_id = $product_id;
+                    }elseif (in_array($variation_id, $product_ids)) {
+                        $check_id = $variation_id;
+                    } else continue;
+
+
+
+                    $order_date         = $order->get_date_created()->getTimestamp();
+                    $months             = $purchase_data[$check_id]['month'];
+                    $check_time         = strtotime("-$months months");
+                    //echo $order->get_id(), " - ", $order->get_date_created()->format('m-d-Y'), " - ", $item->get_name(), " - ", $item->get_quantity(), " - ", date('Y-m-d', $check_time), "\n";
+
+                    //if variation
+                    if ($order_date>=$check_time) {
+                        //$product = $product
+                        $purchase_data[$check_id]['remain'] = $purchase_data[$check_id]['remain']-$item->get_quantity();
+                        // echo "\n", $order->get_id(), "," ,$item->get_quantity(), " , ", $purchase_data[$check_id]['remain'];
+
+                        if ($purchase_data[$check_id]['remain'] < 0 ) {
+
+                            $errors[$check_id] = $item->get_name();
+                            foreach ($product_ids as $key=>$id) {
+                                if ($id == $check_id) unset($product_ids[$key]);
+                            }
+                            $last_order_date[$check_id] = $order->get_date_created();
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+
+
+
+
+        if (!empty($errors)) {
+            foreach ($errors as $id=>$error) {
+                $errors_obj->add( 'validation', sprintf( __( 'Pemmission denied. Your last item %s was ordered on %s.<br>Per our rules <a href="%s" target="_blank" class="permission_denied">review them here</a> you are eligible for your next %s on %s', 'woocommerce' ), '<strong>' .$error . '</strong>', $last_order_date[$id]->date('m-d-Y'), site_url("/allotments/"), '<strong>' .$error . '</strong>', $last_order_date[$id]->modify("+{$purchase_data[$id]['month']} months")->date('m-d-Y')) );
+            }
+        }
+
+
+    }
+
+    /**
+     * Add tab 'order_limits to product data.
+     */
+    function custom_product_tabs( $tabs) {
+        $tabs['orderlimits'] = array(
+            'label'		=> __( 'Order Limits', 'woocommerce' ),
+            'target'	=> 'order_limits',
+            'class'		=> array( 'show_if_simple', 'show_if_variable'  ),
+
+        );
+        return $tabs;
+    }
+
+    /**
+     * Contents of the order limit product tab.
+     */
+    function order_limits_product_tab_content() {
+        global $post;
+
+        ?>
+        <div id="order_limits" class="panel woocommerce_options_panel">
+            <?php
+
+            // Note the 'id' attribute needs to match the 'target' parameter set above
+
+            woocommerce_wp_checkbox( array(
+                'id' 		=> '_product_rules',
+                'label' 	=> __( 'Product rules', 'woocommerce' ),
+                'description' => ' Allow',
+            ) );
+
+            woocommerce_wp_text_input( array(
+                'id' 		=> '_product_quantity',
+                //'label' 	=> __( 'product quantity', 'woocommerce' ),
+                'description' => $post->post_title . ' per user per',
+                'type'              => 'number',
+                'required' => 'true',
+                'custom_attributes' => array(
+                    'step' 	=> 'any',
+                    'min'	=> '1'
+                ),
+                'style' => 'width:40px;'
+            ) );
+
+            woocommerce_wp_select( array(
+                'id' 		=> '_month',
+                // 'label' 	=> __( 'Product rules', 'woocommerce' ),
+                'description' => 'months',
+                'options' => array_combine(range(1,12), range(1,12)),
+                'style' => 'width:50px;'
+            ) );
+
+            // show this check box if the product has variations
+            echo '<div class="show_if_variable">';
+            woocommerce_wp_checkbox( array(
+                'id' 		=> '_variations',
+                'description' 	=> __( 'Apply to product variations as well', 'woocommerce' )
+            ) );
+            echo '</div>';
+            ?>
+        </div>
+        <?php
+    }
+
+    /**
+     * Save the custom fields.
+     */
+    function save_order_limits_fields( $post_id ) {
+
+
+        $product = wc_get_product( $post_id );
+
+        $product_rules = isset( $_POST['_product_rules'] ) ? 'yes' : 'no';
+        $product->update_meta_data( '_product_rules', $product_rules );
+
+        $product_quantity = isset( $_POST[ '_product_quantity' ] ) ? ( $_POST[ '_product_quantity' ]):'1';
+        $product->update_meta_data('_product_quantity',   $product_quantity);
+
+        $month = isset( $_POST[ '_month' ] ) ? ( $_POST[ '_month' ]):'1';
+        $product->update_meta_data('_month',    $month );
+
+        $product_variations = isset( $_POST['_variations'] ) ? 'yes' : 'no';
+        $product->update_meta_data('_variations',   $product_variations);
+
+        $product->save();
+    }
+    function wp_footer(){
+        if( is_checkout() ){
+            $table = TablePress::$model_table->load( 8, true, true );
+            $string = array();
+            if( sizeof( $table['data']) > 1){
+                $i=0;
+                foreach( $table['data'] as $v){
+                    if( $i != 0 && !empty( $v[0] )){
+                        $string[] = "\"$v[0]\"";
+                    }
+                    $i++;
+                }
+            }
+            ?>
+            <script type="text/javascript">
+                jQuery(document).ready(function($){
+                    var availableTags = [<?php echo implode( ",", $string ); ?>];
+                    $( "#billing_first_name" ).autocomplete({
+                        source: availableTags,
+                        change: function (event, ui) {
+                            if(ui.item === null){
+                                $("#billing_first_name").val("");
+                            }
+
+                        }
+                    });
+                });
+            </script>
+            <?php
+            echo '<style> a.permission_denied, a.permission_denied:visited  {color: #fff;background: #9e0000;padding: 5px;border-radius: 7px;} .woocommerce-error li {margin-bottom: 10px}</style>';
+        }
+    }
     new IZW_Auto_Complete();
+
 }
